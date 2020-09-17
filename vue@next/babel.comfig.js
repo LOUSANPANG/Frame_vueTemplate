f8c836949bf2f7d16ef611d35ccd31aa897ad536
e <1271255653@qq.com>
2020-09-17 17:26:47 +0800

    build: change babel.config

--- vue@next/babel.comfig.js
+++ vue@next/babel.comfig.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-    ['@vue/app', {
-      ployfills: [
-        'es.promise',
-        'es.symbol'
-      ]
-    }]
+    '@vue/cli-plugin-babel/preset'
   ]
 }
