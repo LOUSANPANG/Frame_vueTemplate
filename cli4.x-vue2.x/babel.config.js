21adac217ace0f2364abefeb805fdebc5f5e004b
LOUSANPANG <1271255653@qq.com>
2021-06-08 17:38:10 +0800

    config: :wrench: remove-console babel

--- cli4.x-vue2.x/babel.config.js
+++ cli4.x-vue2.x/babel.config.js
@@ -1,14 +1,29 @@
+// 生产环境
+const prodPlugin = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugin.push([
+    'transform-remove-console',
+    {
+      exclude: ['error', 'warn']
+    }
+  ])
+}
+
 module.exports = {
   "presets": [
     '@vue/cli-plugin-babel/preset'
   ],
+
   "plugins": [
+    // element ui 按需加载
     [
       "component",
       {
         "libraryName": "element-ui",
         "styleLibraryName": "theme-chalk"
       }
-    ]
+    ],
+
+    ...prodPlugin
   ]
 }
