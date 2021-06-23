4046673938b1367bbb4aabc54e1384a6cad324c7
LOUSANPANG <1271255653@qq.com>
2021-06-23 18:14:25 +0800

    test: setup fun

--- vite-ts-vue3/src/exercise/setup.vue
+++ vite-ts-vue3/src/exercise/setup.vue
@@ -1,15 +1,22 @@
 <template>
+  <div>
+
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
+  name: 'Setup',
+
+  setup() {
+
+    return {
+    }
   }
 })
 </script>
 
-<style>
-</style>
+<style scoped></style>
+
