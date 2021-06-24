a7cb92803c523feef8a32c04600e58ad1c3a9dc5
LOUSANPANG <1271255653@qq.com>
2021-06-24 15:47:38 +0800

    test: :white_check_mark: slot

---
+++ vite-ts-vue3/src/exercise/Slot1.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <SlotCom>
+    <template #header>
+      <h2>hi this is header</h2>
+    </template>
+
+    <template #body>
+      <h2>hi this is body</h2>
+    </template>
+
+    <template #foot>
+      <h2>hi this is foot</h2>
+    </template>
+
+    <template #scopeSlot="scopeObj">
+      <h3>{{scopeObj.value}}</h3>
+    </template>
+  </SlotCom>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SlotCom from './Slot2.vue'
+
+export default defineComponent({
+  name: 'Slot1',
+  components: {
+    SlotCom
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ vite-ts-vue3/src/exercise/Slot2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>i am slot component</h1>
+
+    <slot name="header" />
+    <slot name="body" />
+    <slot name="foot" />
+
+    <slot name="scopeSlot" value="i am scope slot" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Slot2'
+})
+</script>
+
+<style scoped></style>
