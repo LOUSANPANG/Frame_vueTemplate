d6cbd997c5177fbe898c8da45acc3343a46be55b
LOUSANPANG <1271255653@qq.com>
2021-06-25 09:27:57 +0800

    test: :white_check_mark: test defineAsyncComponent

---
+++ vite-ts-vue3/src/exercise/DefineAsyncComponent1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <DefineAsyncComponent2 />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineAsyncComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DefineAsyncComponent1',
+  components: {
+    DefineAsyncComponent2: defineAsyncComponent(() =>
+      import('./DefineAsyncComponent2.vue')
+    )
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ vite-ts-vue3/src/exercise/DefineAsyncComponent2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    i am async!
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DefineAsyncComponent2'
+})
+</script>
+
+<style>
+</style>
