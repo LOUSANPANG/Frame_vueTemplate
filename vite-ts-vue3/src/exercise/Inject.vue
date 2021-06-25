b9d5e3a6959f76bfc81c9cd789bcd1194e965528
LOUSANPANG <1271255653@qq.com>
2021-06-25 09:28:17 +0800

    test: :white_check_mark: test provide inject

---
+++ vite-ts-vue3/src/exercise/Inject.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Inject',
+  inject: ['provideVal'],
+  created() {
+    console.log(this.provideVal)
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ vite-ts-vue3/src/exercise/Provide.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <InjectCom />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import InjectCom from './Inject.vue'
+
+export default defineComponent({
+  name: 'Provide',
+  components: {
+    InjectCom
+  },
+  data() {
+    return {
+      todos: [1, 2, 3, 4, 5]
+    }
+  },
+  provide() {
+    return {
+      provideVal: this.todos.length
+    }
+  }
+})
+</script>
+
+<style scoped></style>
