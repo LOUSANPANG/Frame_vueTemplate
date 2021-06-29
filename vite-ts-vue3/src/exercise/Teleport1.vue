2c68d8bd0eecad8f0e94f0711dc3178b5d2e48c7
LOUSANPANG <1271255653@qq.com>
2021-06-29 16:04:46 +0800

    test: :white_check_mark: teleport

---
+++ vite-ts-vue3/src/exercise/Teleport1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="teleport1">
+    <Teleport to="body">
+      <p>1111111</p>
+    </Teleport>
+    <p>2222222</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Teleport1'
+})
+</script>
+
+<style scoped></style>
