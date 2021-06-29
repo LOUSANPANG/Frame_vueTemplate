c3eb6e1c62e2f2f19a17caf7e4164d9c7b3acf1e
LOUSANPANG <1271255653@qq.com>
2021-06-29 14:49:45 +0800

    test: :white_check_mark: directives

---
+++ vite-ts-vue3/src/exercise/Setup7.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-focus>
+    <h2 v-hh:[aa]="100">directives</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Setup7',
+
+  directives: {
+    focus: {
+      mounted(el) {
+        console.log(el)
+        el.focus()
+      }
+    },
+    hh: {
+      mounted(el, { arg, value }) {
+        console.log(el, arg, value)
+      }
+    }
+  },
+
+  setup() {
+    const aa = ref('aa')
+
+    return {
+      aa
+    }
+  }
+})
+</script>
+
+<style scoped></style>
