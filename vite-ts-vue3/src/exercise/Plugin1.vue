cec2bb411fc9f794bc1933bbcb20331aec3e4015
LOUSANPANG <1271255653@qq.com>
2021-06-29 19:41:43 +0800

    test: :white_check_mark: plugin

---
+++ vite-ts-vue3/src/exercise/Plugin1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Plugin1'
+})
+</script>
+
+<style scoped></style>
