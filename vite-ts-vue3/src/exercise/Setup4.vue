273d9b5bb224f43fdb0fbb47d2e1ab0d2f20553d
LOUSANPANG <1271255653@qq.com>
2021-06-29 10:59:49 +0800

    test: :white_check_mark: setup provide inject

---
+++ vite-ts-vue3/src/exercise/Setup4.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Setup5 />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, provide, readonly, reactive } from 'vue'
+import Setup5 from './Setup5.vue'
+
+export default defineComponent({
+  name: 'setup4',
+  components: { Setup5 },
+
+  setup() {
+    onMounted(() => console.log('onMounted'))
+
+    provide('provide1', 11111)
+    provide('provide2', { name: 'lgh' })
+
+    provide('provide3', readonly('33333'))
+
+    const provide4 = reactive({ key: 44444 })
+    provide('provide4', provide4)
+
+    const updateProvide4 = () => {
+      provide4.key = 4
+    }
+    provide('provide5', updateProvide4)
+
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ vite-ts-vue3/src/exercise/Setup5.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>{{inject1}}</h2>
+    <h2>{{name}}</h2>
+    <h2>{{inject4}}</h2>
+
+    <button @click="inject5">update provide4</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  name: 'setup5',
+
+  setup() {
+    const inject1 = inject('provide1', 22222)
+    const { name } = inject('provide2')
+
+    const inject4 = inject('provide4')
+    const inject5 = inject('provide5')
+
+    return {
+      inject1,
+      name,
+      inject4,
+      inject5
+    }
+  }
+})
+</script>
+
+<style scoped></style>
