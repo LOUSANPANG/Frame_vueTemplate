b70b70395aa61dc5133e32a91bce065c897cc8f3
LOUSANPANG <1271255653@qq.com>
2021-06-29 13:24:13 +0800

    test: :white_check_mark: watchEffect

---
+++ vite-ts-vue3/src/exercise/Setup6.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p ref="root">get ref ele</p>
+    <p ref="root1">get ref1 ele</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, watch, watchEffect } from 'vue'
+
+export default defineComponent({
+  name: 'Setup6',
+
+  setup() {
+    const root = ref(null)
+    const root1 = ref(null)
+
+    onMounted(() => {
+      console.log(root.value)
+    })
+
+    watchEffect(() => console.log(root1.value),
+      { flush: 'post' }
+    )
+
+    return {
+      root,
+      root1
+    }
+  }
+})
+</script>
+
+<style scoped></style>
