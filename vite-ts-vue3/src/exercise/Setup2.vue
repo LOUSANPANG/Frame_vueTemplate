f12c46d06c07a163ea4376d47e1b7e5d92c3b0a7
LOUSANPANG <1271255653@qq.com>
2021-06-29 10:27:43 +0800

    test: :white_check_mark: toRef reactive h

--- vite-ts-vue3/src/exercise/Setup2.vue
+++ vite-ts-vue3/src/exercise/Setup2.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Setup1 user="setup2" />
+
+    <Setup3 info="1111" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Setup1 from './Setup1.vue'
+import Setup3 from './Setup3.vue'
 
 export default defineComponent({
   name: 'Setup2',
-  components: { Setup1 },
+  components: { Setup1, Setup3 },
 
   setup() {
     return {
---
+++ vite-ts-vue3/src/exercise/Setup3.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, toRef, reactive, h } from 'vue'
+
+export default defineComponent({
+  name: 'Setup3',
+  props: {
+    info: {
+      type: String
+    },
+    user: {
+      type: String,
+      require: true,
+      default: 'LOUSANPANG'
+    }
+  },
+
+  setup(props, { attrs, slots, emit }) {
+    const info = toRef(props, 'info')
+    const { user } = toRefs(props)
+    console.log(info.value)
+    console.log(user.value)
+
+    console.log(attrs, slots, emit)
+
+    const book = reactive({ title: 'Book Title' })
+    console.log(book)
+
+    return () => h('div', ['h: ', book.title])
+  }
+})
+</script>
+
+<style scoped></style>
