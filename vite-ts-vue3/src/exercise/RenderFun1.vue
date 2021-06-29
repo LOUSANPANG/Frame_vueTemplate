e7fc9d8d2cd9e65f72e2cbabe55c9984b30493ea
LOUSANPANG <1271255653@qq.com>
2021-06-29 19:33:38 +0800

    test: :white_check_mark: render fun

---
+++ vite-ts-vue3/src/exercise/RenderFun1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+
+export default defineComponent({
+  name: 'RenderFun1',
+
+  setup() {
+    return () => h('div',
+      {
+        class: 'aaa'
+      },
+      [
+        'render h',
+        h('p', 'render h render children h')
+      ]
+    )
+  }
+})
+</script>
+
+<style scoped></style>
