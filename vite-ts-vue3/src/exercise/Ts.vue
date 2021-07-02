e7a7f70b83eafa1cc2ea9cf82b4b5912451f9c5d
LOUSANPANG <1271255653@qq.com>
2021-07-02 17:32:03 +0800

    test: :white_check_mark: ts

---
+++ vite-ts-vue3/src/exercise/Ts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+
+export default defineComponent({
+  name: 'Ts',
+
+  setup() {
+    onMounted(() => {
+      const list: Array<number> = [1, 2]
+      console.log(list)
+
+      enum Color {
+        Red,
+        Green
+      }
+      const color: string = Color[1]
+      console.log(color)
+    })
+  }
+})
+</script>
+
+<style scoped></style>
