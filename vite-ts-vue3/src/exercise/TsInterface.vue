b0843057e1eac7e434896311b9eb0ce9856e6673
LOUSANPANG <1271255653@qq.com>
2021-07-05 13:40:33 +0800

    test: ts interface

---
+++ vite-ts-vue3/src/exercise/TsInterface.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+
+export default defineComponent({
+  name: 'TsInterface',
+
+  setup() {
+    onMounted(() => {
+      let list: Array<number> = [1, 2]
+      let list2: number[] = [1, 2]
+
+      interface Point {
+        color?: string,
+        readonly size: number,
+        list: ReadonlyArray<number>,
+        [propName: string]: any
+      }
+
+      interface Fun {
+        (color: string, size: number): boolean
+      }
+
+      interface NumberArray {
+        [index: number]: string
+      }
+
+      interface FunClock {
+        currentTime: Date,
+        setTimer(d: Date): void
+      }
+      class Clock implements FunClock {
+        currentTime: Date
+        setTimer(d: Date) {
+          this.currentTime = d
+        }
+        constructor() {}
+      }
+
+      interface A {
+        color: string
+      }
+      interface B {
+        size: number
+      }
+      interface C extends A, B {
+        popular: string
+      }
+      let c = <C>{}
+    })
+  }
+})
+</script>
+
+<style scoped></style>
